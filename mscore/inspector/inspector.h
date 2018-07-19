e289e7c21e6b2527cda0ebd26477974e01917188
ws <ws@wschweer.de>
2018-07-19 13:03:25 +0200

    fix #264594 lyrics update

--- mscore/inspector/inspector.h
+++ mscore/inspector/inspector.h
@@ -341,7 +341,6 @@ class InspectorLyric : public InspectorTextBase {
       Ui::InspectorLyric l;
 
    private slots:
-      virtual void valueChanged(int idx) override;
 
    public:
       InspectorLyric(QWidget* parent);
